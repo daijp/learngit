773c0fa39c102ad3197622b30089df59828d7187
daijp <daijp@cmmobi.com>
2016-04-22 16:00:21 +0800

    git log

--- git.sql
+++ git.sql
@@ -1,15 +1,24 @@
+设置用户名、邮箱
 git config --global user.name "daijp"
 git config --global user.email "daijp@cmmobi.com"
 
-
+创建git
 git init
 
-
+添加文件
 git add git.sql
 
+提交文件
 git commit -m "test git"
 
+文件状态
 git status
 
+比较文件不同
 git diff
 
+查看日志
+git log
+
+在一行查看日志
+git log --pretty=oneline
