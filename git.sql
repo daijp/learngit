b56c502e62fabd3d637261063644bd7a11fa5d4b
daijp <daijp@cmmobi.com>
2016-04-22 15:53:27 +0800

    test git change

--- git.sql
+++ git.sql
@@ -1,5 +1,15 @@
-$ git config --global user.name "daijp"
-$ git config --global user.email "daijp@cmmobi.com"
+git config --global user.name "daijp"
+git config --global user.email "daijp@cmmobi.com"
 
 
-$ git init
+git init
+
+
+git add git.sql
+
+git commit -m "test git"
+
+git status
+
+git diff
+
